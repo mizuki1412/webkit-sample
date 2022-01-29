b4b56c5286429b033c51edb6eab826a1268b5462
yingchaojie <yingchaojie@163.com>
2022-01-29 19:47:11 +0800

    fix

--- lib/components-layout/user-center.vue
+++ lib/components-layout/user-center.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="flex flex-col justify-center items-center gap-2">
-    <div class="w-full text-center mb-4">欢迎，<span class="font-bold text-xl">{{ storeUserInfo.user ? storeUserInfo.user.name : '--' }}</span></div>
+    <div class="w-full flex flex-col items-center mb-4">
+      <div>欢迎</div>
+      <div class="font-bold text-xl mt-1">{{ storeUserInfo.user ? storeUserInfo.user.name : '--' }}</div>
+    </div>
     <div class="w-full"><el-button class="w-full" type="primary">修改密码</el-button></div>
     <div class="w-full"><el-button class="w-full" type="danger" @click="logout">退出登录</el-button></div>
   </div>
