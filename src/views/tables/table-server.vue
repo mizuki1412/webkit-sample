e755c46253e2f10e248414ff8e07d979635a0f5e
ycj <yingchaojie@163.com>
2024-02-03 17:32:24 +0800

    update table-server

--- src/views/tables/table-server.vue
+++ src/views/tables/table-server.vue
@@ -1,48 +1,49 @@
 <template>
-  <a-spin :spinning="loading">
-    <a-table
-        :data-source="list" sticky
-        :columns="columns"
-        :expand-column-width="100" row-key="name">
-      <template #expandedRowRender="{ record }">
-        {{record.name}}
+  <a-table
+      :data-source="listShow" sticky
+      :columns="columns"
+      :pagination="pagination"
+      :loading="loading"
+      @change="handleTableChange"
+      row-key="name">
+    <template #expandedRowRender="{ record }">
+      {{record.name}}
+    </template>
+    <template #bodyCell="{ column, record }">
+      <template v-if="column.key === 'type'">
+        <div v-if="record.type===1">课程</div>
+        <div v-else>活动</div>
       </template>
-      <template #bodyCell="{ column, record }">
-        <template v-if="column.key === 'type'">
-          <div v-if="record.type===1">课程</div>
-          <div v-else>活动</div>
-        </template>
-        <template v-else-if="column.key === 'start'">
-          <div v-if="record.start">{{ formatDateTime(record.start) }}</div>
-        </template>
-        <template v-else-if="column.key === 'action'">
-          <div class="flex gap-1">
-            <a-button size="small" type="primary">
+      <template v-else-if="column.key === 'start'">
+        <div v-if="record.start">{{ formatDateTime(record.start) }}</div>
+      </template>
+      <template v-else-if="column.key === 'action'">
+        <div class="flex gap-1">
+          <a-button size="small" type="primary">
+            <template #icon>
+              <InfoOutlined />
+            </template>
+          </a-button>
+          <a-popconfirm title="确认删除?" @confirm="remove(record)">
+            <a-button size="small" danger>
               <template #icon>
-                <InfoOutlined />
+                <DeleteFilled />
               </template>
             </a-button>
-            <a-popconfirm title="确认删除?" @confirm="remove(record)">
-              <a-button size="small" danger>
-                <template #icon>
-                  <DeleteFilled />
-                </template>
-              </a-button>
-            </a-popconfirm>
-          </div>
-        </template>
+          </a-popconfirm>
+        </div>
       </template>
-      <template #customFilterDropdown="params">
-        <kit-table-custom-filter :params="params" />
-      </template>
-    </a-table>
-  </a-spin>
+    </template>
+    <template #customFilterDropdown="params">
+      <kit-table-custom-filter :params="params" />
+    </template>
+  </a-table>
 </template>
 <script setup>
-import {ref, onMounted} from 'vue';
+import {ref, onMounted, computed} from 'vue';
 import {useRouter} from "vue-router";
 import {useLoading} from "/lib/service";
-import {formatDateTime} from "../../../lib/utils";
+import {formatDateTime, sleep} from "../../../lib/utils";
 import {DeleteFilled, InfoOutlined, SearchOutlined} from '@ant-design/icons-vue';
 import KitTableCustomFilter from "../../../lib/components/table/kit-table-custom-filter.vue";
 
@@ -52,34 +53,48 @@ const columns = [
   {
     title: '名称', dataIndex: 'name', key: 'name', fixed: true, width: '300px',
     sorter: {compare: (a, b) => a.name >= b.name},
+    customFilterDropdown: true,
   },
   {title: '值', dataIndex: 'val', key: 'val', sorter: (a, b) => a.val >= b.val},
   {
     title: '类型', dataIndex: 'type', key: 'type',
-    customFilterDropdown: true,
-    onFilter: (value, record) => {
-      let s = "活动";
-      if (record.type === 1) {
-        s = "课程"
-      }
-      return s.includes(value.toLowerCase())
-    }
   },
   {title: '开始时间', dataIndex: 'start', key: 'start'},
   {title: '操作', key: 'action', fixed: 'right', width: '100px'},
 ]
+// 原始数据集
 const list = ref([])
-const selectType = ref()
-const selectTime = ref()
+const listShow = ref([])
+
+// 控制当前的pagination
+const pagination = ref({
+  total: 200,
+  current: 1,
+  pageSize: 10,
+});
 
 async function remove(row) {
-  // useConfirm(
-  //   '确认删除？', async () => {
-  //   await useLoading(loading, async function () {
-  //
-  //   })()
-  // })
-  console.log(row)
+
+}
+
+// 调接口
+// filters: {columnKey:xxx}
+// sorter: {columnKey, order='ascend/descend'}
+async function handleTableChange(pag={current:1, pageSize:10}, filters, sorter){
+  loading.value = true
+  await sleep(1000)
+  console.log(pag, filters, sorter)
+  // api
+  let res = list.value
+  for(let k of Object.keys(filters)){
+    // console.log(k, f[k], f[k][0])
+    res = res.filter(x=>x[k].indexOf(filters[k][0])>-1)
+  }
+  pagination.value.total = res.length
+  res = res.slice((pag.current-1)*pag.pageSize, (pag.current)*pag.pageSize)
+  listShow.value = res
+  pagination.value.current = pag.current
+  loading.value = false
 }
 
 onMounted(useLoading(loading, async () => {
@@ -91,6 +106,8 @@ onMounted(useLoading(loading, async () => {
       start: new Date()
     })
   }
+  pagination.value.total = list.value.length
+  listShow.value = list.value.slice(0,10)
 }))
 
 </script>
